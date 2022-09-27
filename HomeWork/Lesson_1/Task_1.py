02aab0f2f31d08dfa74cfeeb288575a998d14e7c
Max Shulga <shulgamackim@gmail.com>
2022-09-27 09:11:08 +0300

    lesson_1 done

--- HomeWork/Lesson_1/Task_1.py
+++ HomeWork/Lesson_1/Task_1.py
@@ -6,5 +6,12 @@
 # - 7 -> да
 # - 1 -> нет
 
-for i in range(7):
-    if i==6 or i==7
+# -------------------------------------------------------------------------------------------------------------
+
+x = int(input('enter the number of the day of the week: '))
+if 0 >= x or x > 7:
+    print('No day with this number')
+elif x == 6 or x == 7:
+    print('yes')
+else:
+    print('no')
